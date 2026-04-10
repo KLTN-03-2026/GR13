bcaa546a2a85a482bc5d53750eef917b408d4e40
Phan Thế Đạt <thedat05.2004@gmail.com>
2026-04-10 23:13:47 +0700

    Done Feature-Admin

---
+++ Beauty_Care_BE/src/controllers/admin.ts
@@ -0,0 +1,12 @@
+import * as services from "../services";
+import { InternalServearError } from "../middlewares/handle_error";
+import { Request, Response } from "express";
+
+export const getDashboardStats = async (req: Request, res: Response) => {
+  try {
+    const response = await services.getDashboardStats();
+    return res.status(200).json(response);
+  } catch (error) {
+    return InternalServerError(res);
+  }
+};
