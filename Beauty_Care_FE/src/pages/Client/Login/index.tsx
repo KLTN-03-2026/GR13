6160a20ad8e1f0c5c7e7d417d47a813bd41b6a62
vanquochoai123 <haoace12@gmail.com>
2026-04-18 11:25:19 +0700

    login and register

---
+++ Beauty_Care_FE/src/pages/Client/Login/index.tsx
@@ -0,0 +1,9 @@
+import LoginComponent from "../../../components/Client/Login/index.tsx";
+const LoginPage = () => {
+  return (
+    <>
+      <LoginComponent />
+    </>
+  );
+};
+export default LoginPage;
